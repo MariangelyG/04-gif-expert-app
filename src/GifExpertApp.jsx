6c91a81a7580c6c44f778f16306e478c6cd86e1b
MariangelyG <mariangelygonzalez90@gmail.com>
2023-09-15 20:00:32 -0300

    "custom hook"

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,43 +1,29 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
-
-
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () =>{
-
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
-
+    const [categories, setCategories] = useState(['One Punch'])
     const onAddCategory = (newCategory) =>{
-    //    console.log(newCategory)
+        if ( categories.includes(newCategory)) return;
         setCategories([newCategory, ...categories])
     }
 
     return (
         <>
-            {/* titulo */}
             <h1>GifExpertApp</h1>
-
-            {/* Input */}
             <AddCategory 
-                // setCategories={ setCategories }
                 onNewCategory={(event) => onAddCategory(event)}
             />
-
-            {/* listado de Gif */}
-                <button onClick={onAddCategory}>Agregar</button>
-                <ol>
-                    {
-                    categories.map(category=> {
-                        return <li key={category}> {category} </li>
-                    
-                     })
-                    }
-                </ol>
-
-
-                {/* Gif Item */}
-
-
+                {
+                categories.map(category=>(
+                    <GifGrid 
+                        key={category}
+                        category={category}
+                    />
+                    ))
+                }
+              
         </>
         )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifts = async (category) =>{
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=BGzjCfS2sI6sNL8Ouwx2nRSPliHjwsMr&q=${category}&limit=10`
+    const resp = await fetch(url);
+    const { data } = await resp.json()
+ 
+    const gifs = data.map(img => ({
+         id: img.id,
+         title: img.title,
+         url: img.images.downsized_medium.url
+ 
+ }))
+    return gifs
+ 
+ }
